4f4fba971eba811f14ca5ee75f34e2f9572b165a
Juan Pablo Gama <juanpablo.gama.olivares@gmail.com>
2023-09-10 15:55:29 -0600

    changes

--- binaryTree.py
+++ binaryTree.py
@@ -4,31 +4,37 @@ class BinaryTree:
     def __init__(self, root):
         self.root = Node(root)
 
-    def add_child(self, new_node):
-        if self.root.left is None:
-            self.root.left = new_node
-        elif self.root.right is None:
-            self.root.right = new_node
+    """ def find(self, data):
+        return self._find(self.root, data)
 
-    def find_node(self, data):
-        return self._find_node(self.root, data)
-
-    def _find_node(self, current_node, data):
+    def _find(self, current_node, data):
         if current_node is None:
+            print("the tree is empty")
             return None
-        if current_node.data == data:
+        elif current_node.data == data:
             return current_node
-        left_result = self._find_node(current_node.left, data)
+        left_result = self._find(current_node.left, data)
         if left_result is not None:
             return left_result
-        return self._find_node(current_node.right, data)
-      
-    def print_tree(self):
-        if self.root is not None:
-            self._print_tree(self.root)
+        return self._find(current_node.right, data) """
 
-    def _print_tree(self, current_node):
-        if current_node is not None:
-            print(current_node.data)
-            self._print_tree(current_node.left)
-            self._print_tree(current_node.right)
+    def _find(self, current_node, data, path=[]):
+        if current_node is None:
+            print("El árbol está vacío")
+            return None, path
+        elif current_node.data == data:
+            path.append(current_node.data)
+            return current_node, path
+        left_result, left_path = self._find(current_node.left, data, path + [current_node.data])
+        if left_result is not None:
+            return left_result, left_path
+        right_result, right_path = self._find(current_node.right, data, path + [current_node.data])
+        return right_result, right_path
+
+    def find(self, data):
+        result, path = self._find(self.root, data)
+        
+        if result is not None:
+            print(f"Node data '{data}' found in track: {path}")
+        else:
+            print(f"Node data '{data}' not found in tree")
--- node.py
+++ node.py
@@ -3,3 +3,9 @@ class Node:
     self.data = data;
     self.left = None
     self.right = None
+    
+  def insert(self, node):
+        if self.left is None:
+            self.left = node
+        elif self.right is None:
+            self.right = node
